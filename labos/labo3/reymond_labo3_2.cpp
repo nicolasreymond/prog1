70e9f36d2abe8503937849e5368b1b7cf31b9a44
nicolasreymond <n.reymond.12@gmail.com>
2024-09-19 11:13:25 +0200

    [Refactor] time duration calculation and update user prompts to French

--- labos/labo3/reymond_labo3_2.cpp
+++ labos/labo3/reymond_labo3_2.cpp
@@ -10,39 +10,32 @@
  */
 
 #include <iostream>
-
 using namespace std;
-
-// Function to calculate the difference between two times
-void calculate_duration(int start_h, int start_m, int end_h, int end_m) {
-    int start_total = start_h * 60 + start_m;  // Start time in minutes
-    int end_total = end_h * 60 + end_m;        // End time in minutes
-
-    // If the start time is greater than the end time, it means the time is over two days
-    if (start_total > end_total) {
-        end_total += 24 * 60;  // Add 24 hours in minutes to the end time
-    }
-
-    int duration = end_total - start_total;
-    int duration_h = duration / 60;
-    int duration_m = duration % 60;
-
-    // Display the result
-    cout << "The duration is: " << duration_h << " hour(s) and " << duration_m << " minute(s)\n";
-}
 
 int main() {
     int start_h, start_m, end_h, end_m;
 
-    // Input for the start time
-    cout << "Enter the start time (hh mm): ";
+    // Entrée de l'heure de début
+    cout << "Entrez l'heure de départ (hh mm): ";
     cin >> start_h >> start_m;
 
-    // Input for the end time
-    cout << "Enter the end time (hh mm): ";
+    // Entrée de l'heure de fin
+    cout << "Entrez l'heure d'arrivée (hh mm): ";
     cin >> end_h >> end_m;
 
-    calculate_duration(start_h, start_m, end_h, end_m);
+    // Convertir les heures et minutes en minutes depuis 00:00
+    int start_total = start_h * 60 + start_m;
+    int end_total = end_h * 60 + end_m;
+
+    // Ajouter 1440 minutes (24 heures) à l'heure d'arrivée pour garantir que le résultat est toujours positif
+    int duration = (end_total - start_total + 1440) % 1440;
+
+    // Calcul des heures et minutes de la durée
+    int duration_h = duration / 60;
+    int duration_m = duration % 60;
+
+    // Afficher le résultat
+    cout << "La durée du trajet est: " << duration_h << " heure(s) et " << duration_m << " minute(s)\n";
 
     return 0;
 }
