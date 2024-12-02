df33078899a9fe8477f62d8411501ea896f8510a
nicolasreymond <n.reymond.12@gmail.com>
2024-12-02 16:20:16 +0100

    Add comprehensive unit tests for addition, subtraction, multiplication, factorial, and Fibonacci functions in test_reymond_labo24.cpp; include exception handling for improved error reporting

--- labos/labo24/reymond_labo24.hpp
+++ labos/labo24/reymond_labo24.hpp
@@ -17,208 +17,160 @@
 using namespace std;
 
 class Unsigned {
-    private:
-        string value;
+private:
+    string value;
 
-    public:
+public:
+    Unsigned(const string &binary) : value(binary) {}
+    Unsigned(uint64_t number) {
+        value = "";
+        // Convert the number to binary
+        while (number > 0) {
+            value = char((number % 2) + '0') + value;
+            number /= 2;
+        }
+        if (value.empty()) {
+            value = "0";
+        }
+    }
 
-        Unsigned(const string &binary) : value(binary) {}
-        Unsigned(uint64_t number)
-        {
-            value = "";
-            // Convert the number to binary
-            while (number > 0)
-            {
-                value = char((number % 2) + '0') + value;
-                number /= 2;
+    string to_decimal() const {
+        uint64_t result = 0;
+        uint64_t power = 1;
+        for (int i = value.size() - 1; i >= 0; --i) {
+            if (value[i] == '1') {
+                result += power;
             }
-            if (value.empty())
-            {
-                value = "0";
-            }
+            power *= 2;
         }
+        return to_string(result);
+    }
 
-        string to_decimal() const
-        {
-            uint64_t result = 0;
-            uint64_t power = 1;
-            for (int i = value.size() - 1; i >= 0; --i)
-            {
-                if (value[i] == '1')
-                {
-                    result += power;
-                }
-                power *= 2;
-            }
-            return to_string(result);
-        }
+    string getValue() {
+        return value;
+    }
 
-        string getValue() {
-            return value;
-        }
+    friend bool operator==(const Unsigned &left, const Unsigned &right) {
+        return left.value == right.value;
+    }
 
-        friend bool operator==(const Unsigned &left, const Unsigned &right)
-        {
-            return left.value == right.value;
-        }
+    Unsigned &operator+=(const Unsigned &right) {
+        int carry = 0;
+        int leftSize = value.size();
+        int rightSize = right.value.size();
+        string result = "";
+        int i = 0;
 
-        Unsigned &operator+=(const Unsigned &right)
-        {
-            int carry = 0;
-            int leftSize = value.size();
-            int rightSize = right.value.size();
-            string result = "";
-            int i = 0;
-
-            while (i < leftSize || i < rightSize || carry != 0)
-            {
-                int leftBit = 0;
-                if (i < leftSize)
-                {
-                    leftBit = value[leftSize - 1 - i] - '0';
-                }
-
-                int rightBit = 0;
-                if (i < rightSize)
-                {
-                    rightBit = right.value[rightSize - 1 - i] - '0';
-                }
-
-                int sum = leftBit + rightBit + carry;
-                result = char((sum % 2) + '0') + result;
-                carry = sum / 2;
-                i++;
+        while (i < leftSize || i < rightSize || carry != 0) {
+            int leftBit = 0;
+            if (i < leftSize) {
+                leftBit = value[leftSize - 1 - i] - '0';
             }
 
-            value = result;
-            return *this;
-        }
-
-        Unsigned &operator-=(const Unsigned &right)
-        {
-            int borrow = 0;
-            int leftSize = value.size();
-            int rightSize = right.value.size();
-            string result = "";
-            int i = 0;
-
-            while (i < leftSize || i < rightSize || borrow != 0)
-            {
-                int leftBit = 0;
-                if (i < leftSize)
-                {
-                    leftBit = value[leftSize - 1 - i] - '0';
-                }
-
-                int rightBit = 0;
-                if (i < rightSize)
-                {
-                    rightBit = right.value[rightSize - 1 - i] - '0';
-                }
-
-                int diff = leftBit - rightBit - borrow;
-                if (diff < 0)
-                {
-                    diff += 2;
-                    borrow = 1;
-                }
-                else
-                {
-                    borrow = 0;
-                }
-
-                result = char(diff + '0') + result;
-                i++;
+            int rightBit = 0;
+            if (i < rightSize) {
+                rightBit = right.value[rightSize - 1 - i] - '0';
             }
 
-            // Remove leading zeros
-            while (result.size() > 1 && result[0] == '0')
-            {
-                result.erase(0, 1);
+            int sum = leftBit + rightBit + carry;
+            result = char((sum % 2) + '0') + result;
+            carry = sum / 2;
+            i++;
+        }
+
+        value = result;
+        return *this;
+    }
+
+    Unsigned &operator-=(const Unsigned &right) {
+        int borrow = 0;
+        int leftSize = value.size();
+        int rightSize = right.value.size();
+        string result = "";
+        int i = 0;
+
+        while (i < leftSize || i < rightSize || borrow != 0) {
+            int leftBit = 0;
+            if (i < leftSize) {
+                leftBit = value[leftSize - 1 - i] - '0';
             }
 
-            value = result;
-            return *this;
-        }
-
-        Unsigned &operator*=(const Unsigned &right)
-        {
-            Unsigned result("0");
-            Unsigned temp = *this;
-
-            for (int i = right.value.size() - 1; i >= 0; --i)
-            {
-                if (right.value[i] == '1')
-                {
-                    result += temp;
-                }
-                temp.value += '0'; // Shift left by 1 bit
+            int rightBit = 0;
+            if (i < rightSize) {
+                rightBit = right.value[rightSize - 1 - i] - '0';
             }
 
-            value = result.value;
-            return *this;
-        }
-
-        friend Unsigned operator/= (Unsigned left, const Unsigned &right)
-        {
-            Unsigned result("0");
-            Unsigned temp("0");
-
-            for (char bit : left.value)
-            {
-                temp.value += bit;
-                while (temp.value.size() > right.value.size() || 
-                       (temp.value.size() == right.value.size() && temp.value >= right.value))
-                {
-                    temp -= right;
-                    result.value += '1';
-                }
-                result.value += '0';
+            int diff = leftBit - rightBit - borrow;
+            if (diff < 0) {
+                diff += 2;
+                borrow = 1;
+            } else {
+                borrow = 0;
             }
 
-            // Remove leading zeros
-            while (result.value.size() > 1 && result.value[0] == '0')
-            {
-                result.value.erase(0, 1);
+            result = char(diff + '0') + result;
+            i++;
+        }
+
+        // Remove leading zeros
+        while (result.size() > 1 && result[0] == '0') {
+            result.erase(0, 1);
+        }
+
+        value = result;
+        return *this;
+    }
+
+    Unsigned &operator*=(const Unsigned &right) {
+        Unsigned result("0");
+        Unsigned temp = *this;
+
+        for (int i = right.value.size() - 1; i >= 0; --i) {
+            if (right.value[i] == '1') {
+                result += temp;
             }
-
-            left.value = result.value;
-            return left;
+            temp.value += '0'; // Shift left by 1 bit
         }
 
+        value = result.value;
+        return *this;
+    }
 
-        friend Unsigned operator+(Unsigned left, const Unsigned &right)
-        {
-            left += right;
-            return left;
+    friend Unsigned operator/=(Unsigned left, const Unsigned &right) {
+        Unsigned result("0");
+        Unsigned temp("0");
+
+        for (char bit : left.value) {
+            temp.value += bit;
+            while (temp.value.size() > right.value.size() ||
+                   (temp.value.size() == right.value.size() && temp.value >= right.value)) {
+                temp -= right;
+                result.value += '1';
+            }
+            result.value += '0';
         }
 
-        friend Unsigned operator-(Unsigned left, const Unsigned &right)
-        {
-            left -= right;
-            return left;
+        // Remove leading zeros
+        while (result.value.size() > 1 && result.value[0] == '0') {
+            result.value.erase(0, 1);
         }
 
-        friend Unsigned operator*(Unsigned left, const Unsigned &right)
-        {
-            left *= right;
-            return left;
-        }
-    };
+        left.value = result.value;
+        return left;
+    }
 
-    // int main(void)
-    // {
-    //     Unsigned u1("1010");    // 10   in binary
-    //     Unsigned u2(10);        // 10   in decimal
-    //     Unsigned u3 = u1 + u2;  // 20   in binary
-    //     Unsigned u4 = u1 - u2;  // 0    in binary
-    //     Unsigned u5 = u1 * u2;  // 100  in binary
+    friend Unsigned operator+(Unsigned left, const Unsigned &right) {
+        left += right;
+        return left;
+    }
 
-    //     cout << "u1 (binary 1010): " << u1.to_decimal() << " in decimal" << endl;
-    //     cout << "u2 (decimal 10): " << u2.getValue() << " in binary" << endl;
-    //     cout << "u3 (u1 + u2): " << u3.to_decimal() << " in decimal" << endl;
-    //     cout << "u4 (u1 - u2): " << u4.to_decimal() << " in decimal" << endl;
-    //     cout << "u5 (u1 * u2): " << u5.to_decimal() << " in decimal" << endl;
-        
-    //     return 0;
-    // }
+    friend Unsigned operator-(Unsigned left, const Unsigned &right) {
+        left -= right;
+        return left;
+    }
+
+    friend Unsigned operator*(Unsigned left, const Unsigned &right) {
+        left *= right;
+        return left;
+    }
+};
--- labos/labo24/test_reymond_labo24.cpp
+++ labos/labo24/test_reymond_labo24.cpp
@@ -1,62 +1,214 @@
-/**
- * @file    test_reymond_labo24.cpp
- * @brief   Unit tests for reymond_labo24.cpp
- * @version 1.0
- * @date    20-09-2024
- */
-
-#include <iostream>
+#include "./reymond_labo24.hpp"
 #include <cassert>
-#include "reymond_labo24.hpp"
+#include <iostream>
+#include <exception>
 
 using namespace std;
 
 void test_addition()
 {
-    Unsigned u1("1010");   // 10 in binary
-    Unsigned u2(10);       // 10 in decimal
-    Unsigned u3 = u1 + u2; // 20 in binary
-    assert(u3.to_decimal() == "20");
+    try
+    {
+        Unsigned u1("1010");   // 10 in binary
+        Unsigned u2(10);       // 10 in decimal
+        Unsigned u3 = u1 + u2; // 20 in binary
+        assert(u3.to_decimal() == "20");
+
+        Unsigned u4("1111");   // 15 in binary
+        Unsigned u5(5);        // 5 in decimal
+        Unsigned u6 = u4 + u5; // 20 in binary
+        assert(u6.to_decimal() == "20");
+
+        // Additional tests
+        Unsigned u7("0");
+        Unsigned u8(0);
+        Unsigned u9 = u7 + u8;
+        assert(u9.to_decimal() == "0");
+
+        Unsigned u10("1001");  // 9 in binary
+        Unsigned u11(1);       // 1 in decimal
+        Unsigned u12 = u10 + u11; // 10 in binary
+        assert(u12.to_decimal() == "10");
+
+        cout << "Addition tests passed!" << endl;
+    }
+    catch (const exception& e)
+    {
+        cerr << "Addition tests failed: " << e.what() << endl;
+    }
 }
 
 void test_subtraction()
 {
-    Unsigned u1("1010");   // 10 in binary
-    Unsigned u2(10);       // 10 in decimal
-    Unsigned u4 = u1 - u2; // 0 in binary
-    assert(u4.to_decimal() == "0");
+    try
+    {
+        Unsigned u1("1010");   // 10 in binary
+        Unsigned u2(10);       // 10 in decimal
+        Unsigned u4 = u1 - u2; // 0 in binary
+        assert(u4.to_decimal() == "0");
+
+        Unsigned u5("1111");   // 15 in binary
+        Unsigned u6(5);        // 5 in decimal
+        Unsigned u7 = u5 - u6; // 10 in binary
+        assert(u7.to_decimal() == "10");
+
+        // Additional tests
+        Unsigned u8("1000");   // 8 in binary
+        Unsigned u9(3);        // 3 in decimal
+        Unsigned u10 = u8 - u9; // 5 in binary
+        assert(u10.to_decimal() == "5");
+
+        Unsigned u11("1010");  // 10 in binary
+        Unsigned u12(0);       // 0 in decimal
+        Unsigned u13 = u11 - u12; // 10 in binary
+        assert(u13.to_decimal() == "10");
+
+        cout << "Subtraction tests passed!" << endl;
+    }
+    catch (const exception& e)
+    {
+        cerr << "Subtraction tests failed: " << e.what() << endl;
+    }
 }
 
 void test_multiplication()
 {
-    Unsigned u1("1010");   // 10 in binary
-    Unsigned u2(10);       // 10 in decimal
-    Unsigned u5 = u1 * u2; // 100 in binary
-    assert(u5.to_decimal() == "100");
+    try
+    {
+        Unsigned u1("1010");   // 10 in binary
+        Unsigned u2(10);       // 10 in decimal
+        Unsigned u5 = u1 * u2; // 100 in binary
+        assert(u5.to_decimal() == "100");
+
+        Unsigned u6("111");    // 7 in binary
+        Unsigned u7(3);        // 3 in decimal
+        Unsigned u8 = u6 * u7; // 21 in binary
+        assert(u8.to_decimal() == "21");
+
+        // Additional tests
+        Unsigned u9("0");
+        Unsigned u10(5);
+        Unsigned u11 = u9 * u10;
+        assert(u11.to_decimal() == "0");
+
+        Unsigned u12("101");   // 5 in binary
+        Unsigned u13(4);       // 4 in decimal
+        Unsigned u14 = u12 * u13; // 20 in binary
+        assert(u14.to_decimal() == "20");
+
+        cout << "Multiplication tests passed!" << endl;
+    }
+    catch (const exception& e)
+    {
+        cerr << "Multiplication tests failed: " << e.what() << endl;
+    }
 }
 
 void test_factorial()
 {
-    Unsigned result("1");
-    for (int i = 1; i <= 5; ++i)
+    try
     {
-        result *= Unsigned(i);
+        Unsigned result("1");
+        int i;
+        for (i = 1; i <= 5; ++i)
+        {
+            result *= Unsigned(i);
+        }
+        assert(result.to_decimal() == "120");
+
+        result = Unsigned("1");
+        for (i = 1; i <= 7; ++i)
+        {
+            result *= Unsigned(i);
+        }
+        assert(result.to_decimal() == "5040");
+
+        // Additional tests
+        result = Unsigned("1");
+        for (i = 1; i <= 0; ++i)
+        {
+            result *= Unsigned(i);
+        }
+        assert(result.to_decimal() == "1");
+
+        result = Unsigned("1");
+        for (i = 1; i <= 3; ++i)
+        {
+            result *= Unsigned(i);
+        }
+        assert(result.to_decimal() == "6");
+
+        cout << "Factorial tests passed!" << endl;
     }
-    assert(result.to_decimal() == "120");
+    catch (const exception& e)
+    {
+        cerr << "Factorial tests failed: " << e.what() << endl;
+    }
 }
 
 void test_fibonacci()
 {
-    Unsigned a("0");
-    Unsigned b("1");
-    Unsigned c("0");
-    for (int i = 0; i < 10; ++i)
+    try
     {
-        c = a + b;
-        a = b;
-        b = c;
+        Unsigned a("0");
+        Unsigned b("1");
+        Unsigned c("0");
+        int i;
+        for (i = 0; i < 10; ++i)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        assert(a.to_decimal() == "55");
+
+        a = Unsigned("0");
+        b = Unsigned("1");
+        for (i = 0; i < 15; ++i)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        assert(a.to_decimal() == "610");
+
+        // Additional tests
+        a = Unsigned("0");
+        b = Unsigned("1");
+        for (i = 0; i < 5; ++i)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        assert(a.to_decimal() == "5");
+
+        a = Unsigned("0");
+        b = Unsigned("1");
+        for (i = 0; i < 20; ++i)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        assert(a.to_decimal() == "6765");
+
+        a = Unsigned("0");
+        b = Unsigned("1");
+        for (i = 0; i < 100; ++i)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        assert(a.to_decimal() == "3736710778780434371");
+
+        cout << "Fibonacci tests passed!" << endl;
     }
-    assert(a.to_decimal() == "55");
+    catch (const exception& e)
+    {
+        cerr << "Fibonacci tests failed: " << e.what() << endl;
+    }
 }
 
 int main(void)
